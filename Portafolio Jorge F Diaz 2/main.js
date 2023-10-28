e68379e8759a27dad93e6292f8cb8442ab9ce624
Fede Díaz <98406183+FedeDiaz33@users.noreply.github.com>
2023-10-28 20:26:57 -0300

    Add files via upload

---
+++ Portafolio Jorge F Diaz 2/main.js
@@ -0,0 +1,20 @@
+
+ScrollReveal().reveal(".hero");
+ScrollReveal().reveal(".hero", { delay: 1000 });
+ScrollReveal().reveal(".container", { delay: 100 });
+
+var updates = 0;
+
+var insertedNode = parentNode.insertBefore(newNode, referenceNode);
+
+anime({
+    targets: '.icon-link',
+    value:360,
+    duration: 8800,
+    rotate: 550,
+    scale: 1.5,
+    easing: 'easeInOutSine',
+    rotate: '2turn'
+  });
+
+
